9496a742f01f01be76649630e37154bd021603ff
Prayas Jain <prayas.jn24@gmail.com>
2020-12-26 17:09:47 +0530

    created server.py

---
+++ server/server.py
@@ -0,0 +1,108 @@
+from flask import Flask
+import atexit
+import json
+import os
+import platform
+import shutil
+import subprocess
+import tempfile
+import time
+import zipfile
+from pathlib import Path
+from threading import Timer
+import requests
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+
+def _get_command():
+    system = platform.system()
+    if system == "Darwin":
+        command = "ngrok"
+    elif system == "Windows":
+        command = "ngrok.exe"
+    elif system == "Linux":
+        command = "ngrok"
+    else:
+        raise Exception("{system} is not supported".format(system=system))
+    return command
+
+def _run_ngrok(port):
+    command = _get_command()
+    ngrok_path = str(Path(tempfile.gettempdir(), "ngrok"))
+    _download_ngrok(ngrok_path)
+    executable = str(Path(ngrok_path, command))
+    os.chmod(executable, 0o777)
+    ngrok = subprocess.Popen([executable, 'http', str(port)])
+    atexit.register(ngrok.terminate)
+    localhost_url = "http://localhost:4040/api/tunnels"  # Url with tunnel details
+    time.sleep(1)
+    tunnel_url = requests.get(localhost_url).text  # Get the tunnel information
+    j = json.loads(tunnel_url)
+    tunnel_url = j['tunnels'][0]['public_url']  # Do the parsing of the get
+    tunnel_url = tunnel_url.replace("https", "http")
+    return tunnel_url
+
+def _download_ngrok(ngrok_path):
+    if Path(ngrok_path).exists():
+        return
+    system = platform.system()
+    if system == "Darwin":
+        url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip"
+    elif system == "Windows":
+        url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip"
+    elif system == "Linux":
+        url = "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip"
+    else:
+        raise Exception(f"{system} is not supported")
+    download_path = _download_file(url)
+    with zipfile.ZipFile(download_path, "r") as zip_ref:
+        zip_ref.extractall(ngrok_path)
+
+def _download_file(url):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    download_path = str(Path(tempfile.gettempdir(), local_filename))
+    with open(download_path, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+    return download_path
+
+def start_ngrok(port):
+    ngrok_address = _run_ngrok(port)
+
+    qtapp = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle('Dummy Multiplayer')
+    window.setGeometry(100, 100, 280, 80)
+    window.move(60, 15)
+    helloMsg = QLabel('<h1>'+ngrok_address.split("/")[-1].split(".")[0]+'</h1>', parent=window)
+    helloMsg.move(60, 15)
+
+    window.show()
+    sys.exit(qtapp.exec_())
+
+    print(f" * Running on {ngrok_address}")
+    print(f" * Traffic stats available on http://127.0.0.1:4040")
+
+def run_with_ngrok(app):
+    old_run = app.run
+    def new_run(*args, **kwargs):
+        port = kwargs.get('port', 5000)
+        thread = Timer(1, start_ngrok, args=(port,))
+        thread.setDaemon(True)
+        thread.start()
+        old_run(*args, **kwargs)
+    app.run = new_run
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route("/")
+def hello():
+    return {'val':"Hello World!"}
+
+if __name__ == '__main__':
+    app.run()
